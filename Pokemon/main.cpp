fdea34b6a7a65426978b27ca36dde14c410d6f68
Shwet Garg <shwet.garg@wundermanthompson.com>
2025-06-06 16:32:46 +0530

    Feature_1_Pokemon_Selection | Create main() function

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    string player_name;
+    
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
 
     return 0;
 }
